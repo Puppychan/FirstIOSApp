7395ac3aa93b7d0914a6e968a6f1ae0a7f5bca8c
Matsuri <nhungmaitran1412@gmail.com>
2022-05-27 11:11:47 +0700

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Nhung Tran on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
